b4d20898b22eea7518ef86ebb6be74b7e2a31855
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-02-23 22:01:09 +0530

    Update Inbox.js

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,13 +1,42 @@
 import React, { useState, useRef } from "react";
 
 const Inbox = (props) => {
+ const [newTask, setNewTask] = useState(false);
+  const titleRef = useRef(null);
+  const dateRef = useRef(null);
+
+  const handleNewTaskClick = () => {
+    setNewTask(true);
+  };
+
+  const handleAddTaskClick = (event) => {
+    event.preventDefault();
+
+    const newTitle = titleRef.current.value;
+    const newDate = dateRef.current.value;
+
+    // Create a new task object and add it to the list
+    const newTask = { title: newTitle, date: newDate };
+    props.addTask(newTask);
+
+    // Reset the form
+    titleRef.current.value = "";
+    dateRef.current.value = "2022-09-27";
+    setNewTask(false);
+  };
+
+  const handleCancelClick = () => {
+    titleRef.current.value = "";
+    dateRef.current.value = "2022-09-27";
+    setNewTask(false);
+  };
  
 
   return (
     <div>
       <h3>Inbox</h3>
       {!newTask && (
-        <button className="new" onClick={} id='add-new'>
+        <button className="new" onClick={handleNewTaskClick} id='add-new'>
           +Add New
         </button>
       )}
@@ -15,10 +44,10 @@ const Inbox = (props) => {
         <form className="newtask-box">
           <input type="text" id="title" ref={}></input>
           <div className="buttons">
-            <button className="new" id="add-list" onClick={}>
+            <button className="new" id="add-list" onClick={handleAddTaskClick}>
               Add Task
             </button>
-            <button className="new" onClick={}>
+            <button className="new" onClick={handleCancelClick}>
               Cancel
             </button>
             <input
@@ -33,9 +62,9 @@ const Inbox = (props) => {
       <div id="inbox">
         {props.list.map((list) => {
           return (
-            <div className="box" key={}>
+            <div className="box" key={index}>
               <div className="task">
-                {} ({})
+                {list.title} ({list.date})
               </div>
             </div>
           );
